e7bb198e0d6907897b466696c099b6a1377080d0
Priyanshu-hawk <priyanshupatel.hawk@gmail.com>
2025-01-28 12:44:39 +0530

    train update

--- train.py
+++ train.py
@@ -1,15 +1,26 @@
 import torch
 from torch import nn
-import torch.nn.functional as F
 
 # INput Dim -> Hidden_dim -> mean, std -> Parametrization trick -> Decoder -> Output ime
 class VAE(nn.Module):
     def __init__(self, input_dim, h_dim=200, z_dim=20):
         super().__init__()
+
+        #encoder
         self.img_2hdim = nn.Linear(input_dim, h_dim)
+        self.hid_2mu = nn.Linear(h_dim, z_dim)
+        self.hid_2sigma = nn.Linear(h_dim, z_dim)
+
+        #decoder
+        self.z_2hid = nn.Linear(z_dim, h_dim)
+        self.hid_2img = nn.Linear(h_dim, input_dim)
+
+        self.relu = nn.ReLU()
 
     def encode(self, x):
-        pass
+        h = self.relu(self.img_2hdim(x))
+        mu, sigma = self.hid_2mu(h), self.hid_2sigma(h)
+        return mu, sigma
 
     def decode(self, z):
         pass
